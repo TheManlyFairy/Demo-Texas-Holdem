2c013b30aac6ef23169da1705649440b79a75c32
Nati <deviln3107@hotmail.com>
2019-08-03 20:16:47 +0300

    Scripts & scenes import
    Imported scripts and scenes from my Photon Test project

---
+++ Assets/Nati/Scripts/GameSetupController.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSetupController : MonoBehaviour
+{
+    Vector3 randomPosition;
+    [SerializeField] GameObject playerPrefab;
+    [SerializeField] Text roomName;
+
+    void Start()
+    {
+        roomName.text = PhotonNetwork.CurrentRoom.Name;
+        CreatePlayer(); 
+    }
+
+  void CreatePlayer()
+    {
+        randomPosition = new Vector3(Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3));
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero + randomPosition, Quaternion.identity);
+        
+    }
+}
