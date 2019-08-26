eb906c01c10b353803c6ec73a80e40e20d8a9d78
Aviv Komemi <komemiav@gmail.com>
2019-08-26 16:24:32 +0300

    Aesthetics Update
    * Added new scene AestheticsScene
    - Contains all the necessary graphics for server side of poker table
    - Chips stack up visually for every image of a chip added!
    - Up to 6 players in a single table, organize neatly by hand.
    
    * Added new script PlayerDisplay.
    - References the players display icon, name, remaining money and total amount bet.
    - Added two simple methods to update player display at startup and at runtime.
    
    * TODO
    - Have the UI Manager update player displays per player.
    - Have the UI Manager disable leftover player displays if less than 6 players joined the room.
    - Create a chip stacking effect.

---
+++ Assets/Scripts/UI/PlayerDisplay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class PlayerDisplay : MonoBehaviour
+{
+    [SerializeField]
+    Image playerIcon;
+    [SerializeField]
+    Text playerName;
+    [SerializeField]
+    Text playerRemainingMoney;
+    [SerializeField]
+    Text playerTotalBet;
+
+    public void SetupPlayer(Player player)
+    {
+        //Assign player icon here
+        playerName.text = player.name;
+        playerRemainingMoney.text = "$" + player.money;
+        playerTotalBet.text = "$0";
+    }
+    public void UpdatePlayerMoney(int totalBet, int remainingMoney)
+    {
+        playerTotalBet.text = "$" + totalBet;
+        playerRemainingMoney.text = "$" + remainingMoney;
+    }
+   
+}
