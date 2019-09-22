b4b5a6a23f6e0fcd6d89c37f1620fff1813181eb
Aviv Komemi <komemiav@gmail.com>
2019-09-22 20:38:26 +0300

    Complete Store Page UI
    *New Scene: Store Front Page
    - Canvas Divided into 3 parts: Login, Loading and Store.
    - Login page currently features faux options to connect with google or face or continue as guest. 
    - Store page features 9 games - one for texas holdem and 8 faux games for appearance. Currently does not actually start a game when hitting "Play".

---
+++ Assets/Scripts/Store/UI/RotateLoaderImage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateLoaderImage : MonoBehaviour
+{
+    RectTransform rectTransform;
+    [SerializeField]
+    float rotateSpeed;
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    void Update()
+    {
+        rectTransform.Rotate(new Vector3(0, 0, rotateSpeed), Space.Self);
+    }
+}
---
+++ Assets/Scripts/Store/UI/UIAnimator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIAnimator : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform indicatorRectTrans;
+    [SerializeField]
+    Button forYou, categories, editorChoice, topChart;
+    [SerializeField]
+    float slideTime = 0.4f;
+    float transformX;
+
+    static IEnumerator slide;
+    private void Start()
+    {
+        forYou.onClick.AddListener(delegate { ShiftIndicator(forYou.GetComponent<RectTransform>().localPosition.x); });
+        categories.onClick.AddListener(delegate { ShiftIndicator(categories.GetComponent<RectTransform>().localPosition.x); });
+        editorChoice.onClick.AddListener(delegate { ShiftIndicator(editorChoice.GetComponent<RectTransform>().localPosition.x); });
+        topChart.onClick.AddListener(delegate { ShiftIndicator(topChart.GetComponent<RectTransform>().localPosition.x); });
+    }
+    public void ShiftIndicator(float transformX)
+    {
+        if (slide != null)
+            StopCoroutine(slide);
+        slide = SlideIndicator(transformX);
+        StartCoroutine(slide);
+    }
+
+    
+    IEnumerator SlideIndicator(float transformX)
+    {
+        float timer = 0;
+        float startX = indicatorRectTrans.localPosition.x;
+        float newPosX;
+        while (timer<=slideTime)
+        {
+            newPosX = Mathf.SmoothStep(startX, transformX, timer / slideTime);
+            indicatorRectTrans.localPosition = new Vector2(newPosX, indicatorRectTrans.localPosition.y);
+            yield return null;
+            timer += Time.deltaTime;
+        }
+    }
+}
